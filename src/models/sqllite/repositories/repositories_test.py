a0c8878967a6b8e9d356ff1a28c7d41a9a620c0d
Gabriel3133 <gabrielvb379@gmail.comgit config --global user.email gabrielvb379@gmail.com>
2026-02-25 13:17:10 -0300

    feat: Implemeting people_repository

--- src/models/sqllite/repositories/repositories_test.py
+++ src/models/sqllite/repositories/repositories_test.py
@@ -1,8 +1,9 @@
 import pytest
 from src.models.sqllite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="interacao com o banco")
@@ -18,3 +19,23 @@ def test_delete_pet ():
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_insert_person():
+    firts_name = "teste name"
+    last_name = "teste last "
+    age = 20
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name=firts_name, last_name=last_name, age=age, pet_id=pet_id)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print()
+    print(response)
+    print(response.pet_name)
