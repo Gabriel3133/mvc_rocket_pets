1bf64c92f6a0c4f2bb4137898aef3d27c51f0d7a
Gabriel3133 <gabrielvb379@gmail.comgit config --global user.email gabrielvb379@gmail.com>
2026-02-27 14:14:21 -0300

    feat: Implementing the rest of the views

--- src/views/interfaces/view_interface.py
+++ src/views/interfaces/view_interface.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from src.views.http_types.http_request import HttpResquest
+from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
 class ViewInterface(ABC):
 
     @abstractmethod
-    def handel(self,http_request: HttpResquest) -> HttpResponse:
+    def handel(self,http_request: HttpRequest) -> HttpResponse:
         pass
--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,5 +1,5 @@
 from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface
-from .http_types.http_request import HttpResquest
+from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from .interfaces.view_interface import ViewInterface
 
@@ -7,7 +7,7 @@ class PersonCreatorView(ViewInterface):
     def __init__(self, controller: PersonCreatorControllerInterface) -> None:
         self.__controller = controller
 
-    def handle (self, http_request: HttpResquest) -> HttpResponse:
+    def handle (self, http_request: HttpRequest) -> HttpResponse:
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
 
