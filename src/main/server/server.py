f3e1b430bc334638c253b2f779d07fbe7ea9ebc2
Gabriel3133 <gabrielvb379@gmail.comgit config --global user.email gabrielvb379@gmail.com>
2026-03-02 10:52:07 -0300

    feat: Implementing project routes for pets and peole

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -4,6 +4,7 @@ from src.models.sqllite.settings.connection import db_connection_handler
 
 # importar blueprints
 from src.main.routes.pets_routes import pet_router_bp
+from src.main.routes.person_routes import person_router_bp
 
 db_connection_handler.connect_to_db()
 
@@ -11,3 +12,4 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(pet_router_bp)
+app.register_blueprint(person_router_bp)
--- src/views/interfaces/view_interface.py
+++ src/views/interfaces/view_interface.py
@@ -5,5 +5,5 @@ from src.views.http_types.http_response import HttpResponse
 class ViewInterface(ABC):
 
     @abstractmethod
-    def handel(self,http_request: HttpRequest) -> HttpResponse:
+    def handle(self,http_request: HttpRequest) -> HttpResponse:
         pass
